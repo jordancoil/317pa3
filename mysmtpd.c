a1e9b13c53cf99a4ebbef95bb368fafa65e920dc
Jordan Coil <albert.jordan.coil@gmail.com>
2021-11-30 13:38:00 -0800

    implementing EHLO and HELO

--- mysmtpd.c
+++ mysmtpd.c
@@ -24,8 +24,10 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-int parse_message(char * out, int fd) {
+int parse_message(char * out, int fd, int client_init, char domain[]) {
 	// TODO: verify that the string ends with \r\n
+
+	// TODO: add command sequence, and "503 bad sequence of commands" if not followed
 
 	// The command will be the first sequence of letters separated by a space
 	// OR, the comand with be followed with a CRLF
@@ -39,25 +41,48 @@ int parse_message(char * out, int fd) {
 		command = strtok(out, crlf);
 	}
 
-	if (strcasecmp(command, "HELO") == 0) {
-		send_formatted(fd, "HELO to you too! \r\n");
-	} else if (strcasecmp(command, "EHLO") == 0) {
-		send_formatted(fd, "EHLO command received! \r\n");
+	if (strcasecmp(command, "HELO") == 0 || strcasecmp(command, "EHLO") == 0) {
+		char *client;
+		client = strtok(strchr(out, ' '), crlf);
+
+		if (client == NULL) {
+			send_formatted(fd, "501 tell me who you are please. \r\n");
+		} else {
+			client_init = 1;
+			send_formatted(fd, "250 %s \r\n", domain);
+		}
 	} else if (strcasecmp(command, "MAIL") == 0) {
+		if (client_init == 0) {
+				send_formatted(fd, "503 bad sequence of commands \r\n");
+		}
 		send_formatted(fd, "MAIL command received! \r\n");
 	} else if (strcasecmp(command, "RCPT") == 0) {
+		if (client_init == 0) {
+				send_formatted(fd, "503 bad sequence of commands \r\n");
+		}
 		send_formatted(fd, "RCPT command received! \r\n");
 	} else if (strcasecmp(command, "DATA") == 0) {
+		if (client_init == 0) {
+				send_formatted(fd, "503 bad sequence of commands \r\n");
+		}
 		send_formatted(fd, "DATA command received! \r\n");
 	} else if (strcasecmp(command, "RSET") == 0) {
+		if (client_init == 0) {
+				send_formatted(fd, "503 bad sequence of commands \r\n");
+		}
 		send_formatted(fd, "RSET command received! \r\n");
 	} else if (strcasecmp(command, "VRFY") == 0) {
 		char *param;
 		param = strtok(strchr(out, ' '), crlf);
-		if (is_valid_user(param, NULL) == 1) {
-			send_formatted(fd, "250 %s \r\n", param);
+
+		if (param == NULL) {
+			send_formatted(fd, "501 please provide a user \r\n");
 		} else {
-			send_formatted(fd, "550 %s is not a valid user \r\n", param);
+			if (is_valid_user(param, NULL) == 1) {
+				send_formatted(fd, "250 %s \r\n", param);
+			} else {
+				send_formatted(fd, "550 %s is not a valid user \r\n", param);
+			}
 		}
 	} else if (strcasecmp(command, "NOOP") == 0) {
 		send_formatted(fd, "250 OK\r\n");
@@ -65,7 +90,7 @@ int parse_message(char * out, int fd) {
 		send_formatted(fd, "221 OK\r\n");
 		return 1; // Return value of 1 means close the connection.
 	} else {
-		send_formatted(fd, "Command \"%s\" is not recognized. \r\n", command);
+		send_formatted(fd, "500 command \"%s\" is not recognized. \r\n", command);
 		// TODO, return 502 for unsupported Command and 500 for unrecognized command
 	}
 
@@ -84,8 +109,9 @@ void handle_client(int fd) {
   /* TO BE COMPLETED BY THE STUDENT */
 	send_formatted(fd, "Welcome\r\n");
 	char out[1024];
+	int client_init = 0;
 	while(nb_read_line(nb, out) > 0) {
-		int close = parse_message(out, fd);
+		int close = parse_message(out, fd, client_init, my_uname.__domainname);
 		if (close) {
 			nb_destroy(nb);
 		}
